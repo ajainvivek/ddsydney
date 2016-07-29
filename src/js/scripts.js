aab347b247a8d2ce0c1aeb5424ae132e4568fb8d
Ajain Vivek <ajainvivek07@gmail.com>
2016-07-29 19:24:43 +0530

    Show modal window on click of image without caption

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,6 +3,26 @@
   'use strict';
 
   $(function () {
+    /**
+    * Open modal on click of image without caption
+    **/
+    $('.image img').on('click', function () {
+      var ele = $(this);
+      if (!ele.data('caption')) {
+        $('#image-modal').modal({
+          show: true,
+          backdrop: 'static',
+          keyboard: false
+        });
+      }
+    });
+
+    /**
+    * Close the modal dialog on click of close button
+    **/
+    $('.modal .close').on('click', function () {
+      $('#image-modal').modal('hide');
+    });
   });
 
 })(jQuery, window, document);
